468547ffd05852a53d303e6f7572be1ea03e9de0
Zyragoon <111765540+Zyragoon@users.noreply.github.com>
2025-05-06 15:35:03 -0500

    Initial commit

---
+++ src/main/java/com/zyra/zynite/Zynite.java
@@ -0,0 +1,16 @@
+package com.zyra.zynite;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Zynite implements ModInitializer {
+	public static final String MOD_ID = "zynite";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/zyra/zynite/ZyniteClient.java
@@ -0,0 +1,10 @@
+package com.zyra.zynite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZyniteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
